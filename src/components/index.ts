1ffd49015e70dca4c0ddffc974f5de8cf98aea8b
Bruno Faria <bruno.fariadevl@gmail.com>
2025-10-10 18:15:58 -0300

    fix: removing unusual file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { AppLayout } from './AppLayout';
 export { Breadcrumbs } from './Breadcrumbs';
 export { ChunkLoadErrorBoundary } from './ChunkLoadErrorBoundary';
-export { CustomSwitch } from './CustomSwitch';
 export { Loading } from './Loading';
 export { NotificationProvider, useNotification } from './NotificationProvider';
 export { PrivacyPolicyModal } from './PrivacyPolicyModal';
